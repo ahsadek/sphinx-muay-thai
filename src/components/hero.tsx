9cc84994f649a2fe6ead8ca620616f4ae62d5803
sadek-cvm <142803097+sadek-cvm@users.noreply.github.com>
2024-08-03 22:13:54 +0300

    hero img update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,17 +1,50 @@
-import { FC } from "react";
-import heroImg from "../assets/heroImg.jpg";
+import { FC, useState, useEffect } from "react";
+import heroImg1 from "../assets/heroImg1.png";
+import heroImg2 from "../assets/heroImg2.png";
 
 interface IHeroProps {}
 
 const Hero: FC<IHeroProps> = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsMobile(window.innerWidth <= 768); // Example breakpoint for mobile
+        };
+
+        checkScreenSize(); // Initial check
+        window.addEventListener("resize", checkScreenSize); // Update on resize
+
+        return () => {
+            window.removeEventListener("resize", checkScreenSize);
+        };
+    }, []);
     return (
-        <div className="fixed w-full flex items-center justify-center mt-24 lg:mt-28">
+        <div className="fixed w-full flex flex-col items-center justify-center mt-24 lg:mt-28">
             <img
-                src={heroImg}
-                className="w-full h-[40rem] lg:h-[54rem] object-cover object-center opacity-10"
+                src={heroImg1}
+                className="mt-6 lg:mt-10 w-full h-[22rem] lg:h-[54rem] object-scale-down opacity-10"
+                style={{
+                    maskImage:
+                        "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0.05) 100%)",
+                    WebkitMaskImage:
+                        "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0.1) 100%)",
+                }}
             />
+            {isMobile && (
+                <img
+                    src={heroImg2}
+                    className="mt-16 lg:mt-10 w-full h-[22rem] lg:h-[54rem] object-scale-down opacity-10"
+                    style={{
+                        maskImage:
+                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0.05) 100%)",
+                        WebkitMaskImage:
+                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0.1) 100%)",
+                    }}
+                />
+            )}
             <div className="flex items-center justify-center absolute font-merienda">
-                <h1 className="text-zinc-200 text-4xl lg:text-5xl tracking-tighter shadow-lg">
+                <h1 className="mt-7 lg:-mt-10 text-zinc-200 text-3xl lg:text-5xl tracking-tighter">
                     The Art Of Eight Limbs
                 </h1>
             </div>
