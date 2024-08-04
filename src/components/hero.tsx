67ab03d52ada33a2aeb5a9016e7ae440955f365c
sadek-cvm <142803097+sadek-cvm@users.noreply.github.com>
2024-08-04 03:46:19 +0300

    another hero update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,7 @@
 import { FC, useState, useEffect } from "react";
 import heroImg1 from "../assets/heroImg1.png";
 import heroImg2 from "../assets/heroImg2.png";
+import heroBg from "../assets/heroBg.svg";
 
 interface IHeroProps {}
 
@@ -20,32 +21,40 @@ const Hero: FC<IHeroProps> = () => {
         };
     }, []);
     return (
-        <div className="fixed w-full flex flex-col items-center justify-center mt-24 lg:mt-28">
+        <div
+            className="fixed w-full flex flex-col items-center justify-center object-contain mt-24 lg:mt-28"
+            style={{
+                backgroundImage: `url(${heroBg})`,
+                backgroundSize: "cover",
+                backgroundPosition: "left",
+                backgroundRepeat: "no-repeat",
+            }}
+        >
             <img
                 src={heroImg1}
-                className="mt-4 lg:mt-10 w-full h-[17.9rem] lg:h-[52rem] object-scale-down opacity-20"
+                className="-ml-12 lg:ml-0 mt-4 lg:mt-10 w-full h-[17rem] lg:h-[52rem] object-scale-down opacity-40"
                 style={{
                     maskImage:
-                        "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0) 100%)",
+                        "linear-gradient(to bottom, rgba(0, 0, 0, 1) 70%, rgba(0, 0, 0, 0) 100%)",
                     WebkitMaskImage:
-                        "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0) 100%)",
+                        "linear-gradient(to bottom, rgba(0, 0, 0, 1) 70%, rgba(0, 0, 0, 0) 100%)",
                 }}
             />
             {isMobile && (
                 <img
                     src={heroImg2}
-                    className="mt-14 lg:mt-10 w-full h-[15.9rem] lg:h-[54rem] object-scale-down opacity-20"
+                    className="ml-16 lg:ml-0 mt-16 lg:mt-10 w-full h-[15.7rem] lg:h-[54rem] object-scale-down opacity-40"
                     style={{
                         maskImage:
-                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0) 100%)",
+                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
                         WebkitMaskImage:
-                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 0) 100%)",
+                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
                     }}
                 />
             )}
             <div className="flex items-center justify-center absolute font-merienda">
-                <h1 className="mt-12 lg:-mt-10 text-zinc-200 text-3xl lg:text-5xl tracking-tighter">
-                    The Art Of Eight Limbs
+                <h1 className="mt-10 lg:-mt-10 text-zinc-200 text-3xl lg:text-5xl tracking-tighter">
+                    The Art Of <span className="text-red-500">Eight</span> Limbs
                 </h1>
             </div>
         </div>
