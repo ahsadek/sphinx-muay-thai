e74c152c8bc685ff446e0e8e617fb0842ed9fae1
sadek-cvm <142803097+sadek-cvm@users.noreply.github.com>
2024-08-04 06:24:46 +0300

    hero final hopefullyl

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,8 @@
 import { FC, useState, useEffect } from "react";
 import heroImg1 from "../assets/heroImg1.png";
 import heroImg2 from "../assets/heroImg2.png";
+import blob1 from "../assets/blob1.svg";
+import blob2 from "../assets/blob2.svg";
 
 interface IHeroProps {}
 
@@ -40,16 +42,26 @@ const Hero: FC<IHeroProps> = () => {
                 }}
             />
             {isMobile && (
-                <img
-                    src={heroImg2}
-                    className="ml-28 lg:ml-0 mt-28 lg:mt-10 w-full h-[15.8rem] lg:h-[54rem] object-scale-down opacity-40"
-                    style={{
-                        maskImage:
-                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
-                        WebkitMaskImage:
-                            "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
-                    }}
-                />
+                <>
+                    <img
+                        src={heroImg2}
+                        className="ml-28 lg:ml-0 mt-28 lg:mt-10 w-full h-[15.8rem] lg:h-[54rem] object-scale-down opacity-40"
+                        style={{
+                            maskImage:
+                                "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
+                            WebkitMaskImage:
+                                "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
+                        }}
+                    />
+                    <img
+                        src={blob1}
+                        className="absolute left-0 h-44 -ml-14 mt-[19rem] rotate-[60deg]"
+                    />
+                    <img
+                        src={blob1}
+                        className="absolute left-0 bottom-0 h-12 mb-7 -ml-3 rotate-[60deg]"
+                    />
+                </>
             )}
             <div className="flex items-center justify-center absolute font-merienda">
                 <h1 className="mt-8 lg:-mt-10 text-zinc-200 text-3xl lg:text-5xl tracking-tighter">
