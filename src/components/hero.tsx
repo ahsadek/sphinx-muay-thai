bdf3af1c7960a5e08a396a9ddff2f1bb6d9ae6cb
sadek-cvm <142803097+sadek-cvm@users.noreply.github.com>
2024-08-04 16:10:19 +0300

    hero update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -32,7 +32,7 @@ const Hero: FC<IHeroProps> = () => {
         >
             <img
                 src={heroImg1}
-                className="-ml-16 -mb-10 lg:ml-0 lg:mb-0 mt-4 lg:mt-10 w-full h-[17rem] lg:h-[52rem] object-scale-down opacity-40"
+                className="-ml-20 -mb-10 lg:ml-0 lg:mb-0 mt-4 lg:mt-10 w-full h-[17rem] lg:h-[52rem] object-scale-down opacity-40"
                 style={{
                     maskImage:
                         "linear-gradient(to bottom, rgba(0, 0, 0, 1) 70%, rgba(0, 0, 0, 0) 100%)",
@@ -44,7 +44,7 @@ const Hero: FC<IHeroProps> = () => {
                 <>
                     <img
                         src={heroImg2}
-                        className="ml-28 lg:ml-0 mt-28 lg:mt-10 w-full h-[15.8rem] lg:h-[54rem] object-scale-down opacity-40"
+                        className="ml-32 lg:ml-0 mt-28 lg:mt-10 w-full h-[15.8rem] lg:h-[54rem] object-scale-down opacity-40"
                         style={{
                             maskImage:
                                 "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
@@ -52,14 +52,17 @@ const Hero: FC<IHeroProps> = () => {
                                 "linear-gradient(to bottom, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
                         }}
                     />
-                    <img
+                    <span className="font-merienda text-[#1D1D20] rotate-90 absolute left-0 bottom-0 text-2xl mb-[8.2rem] -ml-[7.2rem]">
+                        <span className="text-zinc-800">The</span> Art Of Eight Limbs
+                    </span>
+                    {/* <img
                         src={blob1}
                         className="absolute left-0 h-44 -ml-20 mt-[19rem] rotate-[60deg]"
                     />
                     <img
                         src={blob1}
                         className="absolute left-0 bottom-0 h-12 mb-7 -ml-3 rotate-[60deg]"
-                    />
+                    /> */}
                 </>
             )}
             <div className="flex items-center justify-center absolute font-merienda">
